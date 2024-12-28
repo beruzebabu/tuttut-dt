0f3668c1d8a547092229cb5ced64b55004cc8661
beruzebabu <3651741+beruzebabu@users.noreply.github.com>
2024-12-28 17:01:05 +0100

    Add support for specifying tuning via CLI argument

--- tuttut/midi_tabs_cli.py
+++ tuttut/midi_tabs_cli.py
@@ -18,6 +18,7 @@ def parse_args():
     parser = argparse.ArgumentParser(description="MIDI to stringed instrument tabs convertor")
     parser.add_argument("source", metavar="src", type=Path, help="File(path) of MIDI file to convert")
     parser.add_argument("-t", "--target", metavar="target", type=Path, help="Target file(path)", default=None)
+    parser.add_argument("-tu", "--tuning", metavar="tuning", type=str, help="Tuning in string form, from low to high. For example 'D4G4B4E5'", default=None)
     return parser.parse_args()
 
 
@@ -25,13 +26,24 @@ if __name__ == "__main__":
     args = parse_args()
     source: Path = args.source
     target: Path = args.target if args.target is not None else Path(f"./{args.source.with_suffix(".txt")}")
+    tuning: list = []
+    if args.tuning is not None and len(args.tuning) % 2 == 0:
+        tuning = [args.tuning[i*2:(i*2)+2] for i in range(0, len(args.tuning) // 2)]
+        tuning.reverse()
+    elif args.tuning is not None and len(args.tuning) % 2 != 0:
+        print(f"Invalid tuning string ({args.tuning}) supplied, aborting...")
+        raise SystemExit()
+    else:
+        tuning = Tuning.standard_tuning
+
+    print(tuning)
 
     weights = {'b': 1, 'height': 1, 'length': 1, 'n_changed_strings': 1}
 
     try:
         start = time()
         f = pretty_midi.PrettyMIDI(source.absolute().as_posix())
-        tab = Tab(source.stem, Tuning(), f, weights=weights, output_file=target)
+        tab = Tab(source.stem, Tuning(strings=tuning), f, weights=weights, output_file=target)
         # tab = Tab(file.stem, Tuning([Note(69), Note(64), Note(60), Note(67)]), f, weights=weights)
         tab.to_ascii()
         print(f"Time taken: {round(time() - start, 2)}s")
